83032223ed52ef36f935f2fd134a78b0ed79028a
Mathis-Bsnrd <mathisbesnard3@gmail.com>
2021-12-27 14:42:34 +0100

    header et footer ajoutés + package camera download

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Header from './Header/Header';
+import Footer from './Footer/Footer';
+import { Camera } from 'expo-camera';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>test</Text>
-      <StatusBar style="auto" />
-    </View>
+      <>
+        <Header />
+        <View style={styles.container}>
+          <Text>test</Text>
+          <StatusBar style="auto" />
+        </View>
+        <Footer />
+      </>
   );
 }
 
---
+++ Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Footer.css';
+
+
+function Footer() {
+    return (
+        <div className="Footer">
+            <button className="capture">Capture</button>
+        </div>
+    )
+}
+
+export default Footer
