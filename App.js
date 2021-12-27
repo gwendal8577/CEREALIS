6170ab036e102179ed062a6326a7c95027e728cf
Mathis-Bsnrd <mathisbesnard3@gmail.com>
2021-12-27 15:20:39 +0100

    component camera ajouté

--- App.js
+++ App.js
@@ -2,15 +2,15 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Header from './Header/Header';
 import Footer from './Footer/Footer';
-import { Camera } from 'expo-camera';
+import OpenCamera from './camera';
 
 export default function App() {
   return (
       <>
         <Header />
         <View style={styles.container}>
-          <Text>test</Text>
           <StatusBar style="auto" />
+          <OpenCamera />
         </View>
         <Footer />
       </>
@@ -21,7 +21,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
   },
 });
